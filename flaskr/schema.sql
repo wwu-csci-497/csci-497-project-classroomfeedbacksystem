9e396f1f6bdf14e7a2176e4cf059e55f1c54ec99
semj <semj@wwu.edu>
2020-03-05 04:00:55 -0800

    Updated sql schema to match Camille's branch

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -20,17 +20,30 @@ CREATE TABLE question (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   author_id INTEGER NOT NULL,
+  q_type TEXT NOT NULL,
   classname TEXT NOT NULL,
   content TEXT NOT NULL,
   FOREIGN KEY (author_id) REFERENCES user (id)
+);
+
+
+DROP TABLE IF EXISTS options;
+CREATE TABLE options(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  question_id INTEGER NOT NULL,
+  label TEXT,
+  content TEXT,
+  FOREIGN KEY (question_id) REFERENCES question (id)
 );
 
 DROP TABLE IF EXISTS response;
 CREATE TABLE response (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  content TEXT NOT NULL,
+  content TEXT,
+  choice TEXT,
   question_id INTEGER NOT NULL,
   FOREIGN KEY (question_id) REFERENCES question (id)
 );
+
 
